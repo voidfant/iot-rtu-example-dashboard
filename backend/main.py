f211607f9d07623ade40462fd2c7d3b0495286c2
Konstantin Savchenko <nuarielq@yandex.ru>
2023-04-01 12:24:35 +0300

    models modified

--- backend/main.py
+++ backend/main.py
@@ -43,6 +43,12 @@ manager = ConnectionManager()
 def root(request: Request):
     return templates.TemplateResponse("index.htm", {"request": request})
 
+# Package N;
+# Time (s);Altitude (m);
+# Total velocity (m/s);Total acceleration (m/s²);
+# Latitude (°);Longitude (°);
+# Air temperature (°C);Air pressure (mbar)
+
 
 @app.websocket("/api/socket")
 async def websocket_endpoint(websocket: WebSocket):
@@ -50,7 +56,17 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         for msg in consumer:
             raw = msg[6].decode().split(';')
-            payload = {'id': int(raw[0]), 'timestamp': float(raw[1]), 'altitude': float(raw[2])}
+            payload = {
+                        'id': int(raw[0]),
+                        'timestamp': float(raw[1]),
+                        'altitude': float(raw[2]),
+                        'velocity': float(raw[3]),
+                        'acceleration': float(raw[4]),
+                        'latitude': float(raw[5]),
+                        'longitude': float(raw[6]),
+                        'temperature': float(raw[7]),
+                        'pressure': float(raw[8])
+                       }
             print(payload)
             await asyncio.sleep(0.01)
             await manager.broadcast(Message(**payload), websocket)
--- backend/models.py
+++ backend/models.py
@@ -5,3 +5,9 @@ class Message(BaseModel):
     id: int
     timestamp: float
     altitude: float
+    velocity: float
+    acceleration: float
+    latitude: float
+    longitude: float
+    temperature: float
+    pressure: float
